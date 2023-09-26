92180e40e612ae72bd39853159fd41c07c2add3b
viktorzdravkovski <viktorzdravkovski995@gmail.com>
2023-09-26 20:41:43 +0200

    Added Workshop3 and Predavanje 7

---
+++ Predavanje7/react-styling-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomFormCSS from './components/CustomFormCSS';
+import CustomFormInlineStyle from './components/CustomFormInlineStyle';
+
+function App() {
+  return (
+    <div className="app-container">
+      <CustomFormCSS/>
+      <CustomFormInlineStyle/>
+    </div>
+  );
+}
+
+export default App;
